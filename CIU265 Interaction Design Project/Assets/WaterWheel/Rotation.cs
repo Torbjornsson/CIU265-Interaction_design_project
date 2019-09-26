c706762717029cad2694df920ecd4644279f5395
Adam Kjellgren <adam.kjellgren@gmail.com>
2019-09-26 15:06:49 +0200

    Water wheel obstacle

---
+++ CIU265 Interaction Design Project/Assets/WaterWheel/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public int rotationSpeed;
+    void Update (){
+        transform.Rotate (0,0,rotationSpeed*Time.deltaTime); //rotates 50 degrees per second around z axis
+    }
+
+}
