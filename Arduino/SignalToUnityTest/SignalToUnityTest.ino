796d709bf288fd8c664dbcfbc7762dbd323491e9
Miranda <miranda.aldrin@gmail.com>
2019-10-03 14:03:35 +0200

    can now move cube with signals from arduino

--- Arduino/SignalToUnityTest/SignalToUnityTest.ino
+++ Arduino/SignalToUnityTest/SignalToUnityTest.ino
@@ -5,7 +5,7 @@ int right = 0;
 int left = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println("Started");
   pinMode(blueButton, INPUT);
 
---
+++ CIU265 Interaction Design Project/Assets/move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class move : MonoBehaviour
+{
+    SerialPort sp = new SerialPort("/dev/cu.usbmodem1421", 115200);
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        try{
+            string readLine = sp.ReadLine();
+            string goLeft = "go left";
+            string goRight = "go right";
+            print(readLine);
+            if (readLine.Contains(goLeft))
+            {
+                Vector3 position = this.transform.position;
+                position.x--;
+                this.transform.position = position;
+            }
+            else if (readLine.Contains(goRight))
+            {
+                Vector3 position = this.transform.position;
+                position.x++;
+                this.transform.position = position;
+            }
+        }
+        catch(System.Exception){
+        }
+
+        try{
+
+        }
+        catch(System.Exception){
+
+        }
+        
+    }
+}
