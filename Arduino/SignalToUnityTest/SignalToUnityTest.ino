c8b617a25b23b13a23dd699b833f14389ddc4bfd
Miranda <miranda.aldrin@gmail.com>
2019-10-03 12:43:27 +0200

    created a signal test with button and the arduino

---
+++ Arduino/SignalToUnityTest/SignalToUnityTest.ino
@@ -0,0 +1,26 @@
+const int blueButton = 2;
+const int yellowButton = 4;
+
+int right = 0;
+int left = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Started");
+  pinMode(blueButton, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  right = digitalRead(blueButton);
+  left = digitalRead(yellowButton);
+
+  if(right == HIGH){
+    Serial.println("go right");
+  }
+  if(left == HIGH){
+    Serial.println("go left");
+  }
+
+}
