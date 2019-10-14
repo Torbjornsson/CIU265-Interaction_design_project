0825efeb09690b4041f74dae0b5a35223ac3e502
Adam Kjellgren <adam.kjellgren@gmail.com>
2019-10-14 14:20:44 +0200

    Moving platforms, horizontal and vertical

---
+++ CIU265 Interaction Design Project/Assets/HorizontalMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public int top;
+    public float bottom;
+    Vector3 right = new Vector3(1.0f, 0.0f, 0.0f);
+    Vector3 left = new Vector3(-1.0f, 0.0f, 0.0f);
+    private Vector3 moveDirection;
+    float waitUntilTime = -1f;
+    
+    void Update ()
+    {
+        move();
+    }
+    void move () 
+    { 
+        if (transform.position.x >= 10) 
+        {
+            waitUntilTime = Time.time + 10f; //3 second(s) in the future.
+            delay();
+            moveDirection = left; 
+        } 
+        else if (transform.position.x <= 0 ) 
+        {
+            waitUntilTime = Time.time + 10f; //3 second(s) in the future.
+            delay();
+            moveDirection = right;
+        } 
+        
+        transform.Translate (moveDirection * Time.deltaTime * moveSpeed, Space.World);
+    } 
+
+    void delay(){
+          if (Time.time > waitUntilTime) return;
+
+    }
+}
---
+++ CIU265 Interaction Design Project/Assets/VerticalElevator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalElevator : MonoBehaviour
+{
+    public float moveSpeed;
+    public int top;
+    public float bottom;
+    Vector3 upwards = new Vector3(0.0f, 1.0f, 0.0f);
+    Vector3 downwards = new Vector3(0.0f, -1.0f, 0.0f);
+    private Vector3 moveDirection;
+    float waitUntilTime = -1f;
+    
+    void Update ()
+    {
+        move();
+    }
+    void move () 
+    { 
+        if (transform.position.y >= 10) 
+        {
+            waitUntilTime = Time.time + 10f; //3 second(s) in the future.
+            delay();
+            moveDirection = downwards; 
+        } 
+        else if (transform.position.y <= 0 ) 
+        {
+            waitUntilTime = Time.time + 10f; //3 second(s) in the future.
+            delay();
+            moveDirection = upwards;
+        } 
+        
+        transform.Translate (moveDirection * Time.deltaTime * moveSpeed, Space.World);
+    } 
+
+    void delay(){
+          if (Time.time > waitUntilTime) return;
+
+    }
+
+}
