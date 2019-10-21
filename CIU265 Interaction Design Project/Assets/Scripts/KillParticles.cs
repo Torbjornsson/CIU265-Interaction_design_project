dbf05277c85d762545d9d932e918a12a84a4e802
Adam Kjellgren <adam.kjellgren@gmail.com>
2019-10-21 18:56:36 +0200

    WIP: destroy particles

---
+++ CIU265 Interaction Design Project/Assets/Scripts/KillParticles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillParticles : MonoBehaviour
+{   
+    //This shit ain't working.
+    // public ChangeState changeStateScript;
+    // void onStart(){
+    //     changeStateScript = Controller.GetComponent<ChangeState>();
+    // }
+
+    void OnCollisionEnter2D(Collision2D col) 
+    {
+        if(col.gameObject.tag == "Particle")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
